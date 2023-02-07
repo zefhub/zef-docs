ffdc694e1d3f94c3680084e967329c985dea30e4
Daniel Dyrnes <thedanielforum@gmail.com>
2023-02-07 14:21:22 +0900

    Changed algolia config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ module.exports = {
       indexName: "zef-zefhub",
       contextualSearch: true,
       searchParameters: {},
+      pathsToMatch: ["https://zef.zefhub.io/**", "http://zef.zefhub.io/**"],
     },
     navbar: {
       title: "",
